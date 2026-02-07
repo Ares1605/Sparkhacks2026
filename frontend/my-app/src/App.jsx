22a541bf2a83d495c6b51bbd2823349911a0bc9c
l3k4n <alowoojalekan@gmail.com>
2026-02-07 15:10:21 -0600

    define server api for frontend

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Sync from "./pages/sync";
 import Home from "./pages/home"
 import Pref from "./pages/pref"
+import API from "./api/api"
+
 import './App.css'
+import { useMemo } from "react";
 
 function App() {
+  const api = useMemo(() => new API("http://localhost:8080"), [])
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sync" element={<Sync />} />
-        <Route path="/pref" element={<Pref />} />
+        <Route path="/" element={<Home api={api} />} />
+        <Route path="/sync" element={<Sync api={api} />} />
+        <Route path="/pref" element={<Pref api={api} />} />
       </Routes>
     </Router>
   );
