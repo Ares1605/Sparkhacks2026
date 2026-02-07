58d7c44fb70645350545ef97124b48255d740665
IvanG310 <ivangarcia03102006@gmail.com>
2026-02-07 13:20:31 -0600

    Center sidebar, add sparkhacks2026 text, fix height

--- frontend/my-app/src/components/SideBar.jsx
+++ frontend/my-app/src/components/SideBar.jsx
@@ -1,21 +1,12 @@
 import { Link } from "react-router-dom"
 import '../App.css'
-import logo from "frontend/my-app/src/assets/sparkhacks-logo.svg";
 
-import { Link } from "react-router-dom"
-import '../App.css'
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
 
-      <img 
-        src="https://www.sparkhacks.org/sparkhacks-logo.svg"
-        alt="PreCog Logo"
-        style={{ width: "80%", margin: "0 auto", display: "block" }}
-      />
-
-      <h2>PreCog</h2>
+      <h2 style = {{ fontWeight: "800", fontSize: "1.4rem"}}>PreCog</h2>
 
       <p>Navigation</p>
 
@@ -26,6 +17,15 @@ export default function Sidebar() {
       <br />
 
       <Link to="/pref" style={{ color: '#60a5fa', textDecoration: 'none' }}>Pref</Link>
+
+      <img 
+        src="https://www.sparkhacks.org/sparkhacks-logo.svg"
+        alt="PreCog Logo"
+        style={{ width: "80%", margin: "0 auto", display: "block" }}
+      />      
+      <p style={{ marginTop: "12px", fontSize: "0.85rem", opacity: 0.8 }}>
+        SparkHacks <br /> 2026 <br /> @UIC
+      </p>
 
     </div>
   );
