0220a1067334a21f963d31b0bfbf87bc19409fed
japcheno <justinimpoy24@gmail.com>
2026-02-07 12:31:39 -0600

    Save current changes before pulling

--- frontend/my-app/src/components/ProductCard.jsx
+++ frontend/my-app/src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 export default function ProductCard({ name, img, price, isBookmarked, onBookmark }) {
     return (
         <div className="productCard">
-            <img src={img} alt={name} className="product-img"/>
+            <img src={img}   className="product-img"/>
             <h3>{name}</h3>
             <p>{price}</p>
 
