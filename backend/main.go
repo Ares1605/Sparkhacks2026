1e42d9e24b95099c1cafe1b6c179587031713bc2
l3k4n <alowoojalekan@gmail.com>
2026-02-06 21:35:06 -0600

    noop endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func helloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+type Response struct {
+	Msg string `json:"msg"`
+}
+
+
+func noop(w http.ResponseWriter, r *http.Request) {
+	response := Response{"This endpoint does nothing."}
+	if err := json.NewEncoder(w).Encode(&response); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", helloServer)
+	http.HandleFunc("/provider-details", noop)
+	http.HandleFunc("/resync", noop)
+
 	fmt.Println("Server listening on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
